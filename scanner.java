3032d797fa06a7ccaf34ff3593e382c7d87a0791
Chandler Hobby <chandlerhobby@gmail.com>
2021-10-24 13:54:25 -0400

    Update scanner.java

--- scanner.java
+++ scanner.java
@@ -3,23 +3,47 @@ import java.io.*;
 
 public class scanner { //implement main class scanner
     public static void main(String[] args) throws FileNotFoundException { //main method, throws filenotfoundexception
-        File input = new File("C:/Users/chand/OneDrive/Documents/CPL_test.txt"); //Input file designation, includes sample
+        File input = new File("C:\\Users\\chand\\IdeaProjects\\CPL_scanner\\src\\CPL_test"); //Input file designation, includes sample
         Scanner scan = new Scanner(input); //Scanner definition and implementation
 
-        String[] key = new String[] {"LET", "IF", "THEN", "FOR", "TO", "NEXT", "GOTO", "INPUT"}; //Array of keywords and identifiers
-        String[] ident = new String[] {"let", "var", "letter", "num", "if_then", "for_to", "go_to", "input_source", "output_source", "sentence", "word", "expr", "num_expr", "test_expr"};
+        String[] key = new String[] {"LET", "IF_THEN", "FOR_TO", "NEXT", "GOTO", "INPUT", "PRINT", "END"}; //Array of keywords and identifiers
+        ArrayList<String> tokens = new ArrayList<>();
 
         while(scan.hasNextLine()){ //While loop to parse through input file
             String line = scan.nextLine(); //String variable to gather strings from input file
 
             for(String val: line.split(" ")){ //For loop that parses through strings and breaks at whitespaces
-                for(int i = 0 ; i < key.length ; i++){ //For loop that parses through keyword array to check for mathching tokens
-                    if(val.contains(key[i])){ //if statement for validation of matching tokens
-                        System.out.println("Token Scanned: " + val); //print tokens that match
+                for(int i = 0 ; i < key.length ; i++){ //For loop that parses through keyword array to check for matching tokens
+                    if (val.contains("LET")){
+                        tokens.add("LET");
+                        break;
+                    } else if(val.contains("IF")){
+                        tokens.add("IF_THEN");
+                        break;
+                    } else if(val.contains("FOR")){
+                        tokens.add("FOR_TO");
+                        break;
+                    } else if(val.contains("NEXT")){
+                        tokens.add("NEXT");
+                        break;
+                    } else if(val.contains("GO")){
+                        tokens.add("GOTO");
+                        break;
+                    } else if(val.contains("INPUT")){
+                        tokens.add("INPUT");
+                        break;
+                    } else if(val.contains("PRINT")){
+                        tokens.add("PRINT");
+                        break;
+                    } else if(val.contains("END")){
+                        tokens.add("END");
+                        break;
                     }
                 }
             }
         }
+
+        System.out.print("Tokens scanned are: " + tokens);
     }
 }
 
