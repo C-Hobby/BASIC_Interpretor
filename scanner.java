3af9b968838b2e3ff8a5dec209d90397c70410db
Chandler Hobby <chandlerhobby@gmail.com>
2021-10-26 19:54:00 -0400

    Update scanner.java

--- scanner.java
+++ scanner.java
@@ -1,12 +1,10 @@
-import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
 
-/*Need to implement a system for recognizing numbers & letters*/
 
 public class Lexer {
-    public static String[] Keywords = {"LET", "IF", "THEN", "FOR", "TO", "NEXT", "INPUT", "PRINT", "END",
-                                    "VAR", "STARTQUOTE", "ENDQUOTE", "NUM", "+", "-", "*", "/","=","$","%"};
+    public static String[] Tokens = {"LET", "IF", "THEN", "FOR", "TO", "NEXT", "INPUT", "PRINT", "END",
+                                        "+", "-", "*", "/","="};
     public static ArrayList<String> fileContent = new ArrayList<>();
 
     public static void main(String[] args){
@@ -19,15 +17,13 @@ public class Lexer {
         if (readTokens[0] == "INVALID"){
             System.out.print("Cannot read filepath, terminating program...");
             System.exit(0);
-        } else if(readTokens[0] == "SYNTAX ERROR") {
-            System.out.print("Cannot define syntax input, terminating program...");
-            System.exit(0);
         } else {
             System.out.println("Tokens scanned from input: ");
             for(int i = 0 ; i < readTokens.length ; i++){
                 System.out.print(readTokens[i] + "|");
             }
-            System.out.print("\nFile Content: " + fileContent);
+            System.out.print("\n----Will Now Begin Parsing----");
+            Parser.tokenize(fileContent, readTokens);
         }
 
     }
@@ -37,15 +33,12 @@ public class Lexer {
             Scanner fileScan = new Scanner(new File(path));
             List<String> fContent = new ArrayList<>();
             String line = "";
-            int counter = 0;
 
             while(fileScan.hasNextLine()){
-                boolean vLine = true;
                 line = fileScan.nextLine();
-                counter++;
 
                 for(String val : line.split(" ")){
-                    for(int i = 0 ; i < Keywords.length ; i++){
+                    for(int i = 0 ; i < Tokens.length ; i++){
                         if(val.contains("LET")){
                             fContent.add("LET");
                             break;
@@ -70,9 +63,6 @@ public class Lexer {
                         }else if(val.contains("PRINT")){
                             fContent.add("PRINT");
                             break;
-                        }else if(val.contains("\"")){ //This is where I am getting stuck ; I don't need it to print a second STRING token in the final output, just the one
-                            fContent.add("STRING");
-                            break;
                         }else if(val.contains("END")){
                             fContent.add("END");
                             break;
@@ -91,28 +81,16 @@ public class Lexer {
                         }else if(val.contains("=")){
                             fContent.add("=");
                             break;
-                        }else if(val.contains("%")){
-                            fContent.add("%");
-                            break;
-                        }else if(val.contains("$")){
-                            fContent.add("$");
-                            break;
                         } else if(val.matches(".*\\d.*")) {
-                            fContent.add("NUM");
+                            fContent.add(val);
                             break;
                         } else if(val.matches("\\w+")){
-                            fContent.add("VAR");
+                            fContent.add(val);
                             break;
                         }
                     }
                 }
-                if(vLine != true){
-                    System.out.println("Syntactical Error Detected...");
-                    String[] rArray = {"SYNTAX ERROR"};
-                    return rArray;
-                } else {
                     fileContent.add(line);
-                }
             }
 
             fileScan.close();
