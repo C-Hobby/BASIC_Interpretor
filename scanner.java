1ea3e24a4339ee28aed421abee2af071d42ea79f
Chandler Hobby <chandlerhobby@gmail.com>
2021-10-26 16:47:35 -0400

    Update scanner.java

--- scanner.java
+++ scanner.java
@@ -1,49 +1,129 @@
-import java.util.*; //General import statements
+import java.lang.reflect.Array;
+import java.util.*;
 import java.io.*;
 
-public class scanner { //implement main class scanner
-    public static void main(String[] args) throws FileNotFoundException { //main method, throws filenotfoundexception
-        File input = new File("C:\\Users\\chand\\IdeaProjects\\CPL_scanner\\src\\CPL_test"); //Input file designation, includes sample
-        Scanner scan = new Scanner(input); //Scanner definition and implementation
+/*Need to implement a system for recognizing numbers & letters*/
 
-        String[] key = new String[] {"LET", "IF_THEN", "FOR_TO", "NEXT", "GOTO", "INPUT", "PRINT", "END"}; //Array of keywords and identifiers
-        ArrayList<String> tokens = new ArrayList<>();
+public class Lexer {
+    public static String[] Keywords = {"LET", "IF", "THEN", "FOR", "TO", "NEXT", "INPUT", "PRINT", "END",
+                                    "VAR", "STARTQUOTE", "ENDQUOTE", "NUM", "+", "-", "*", "/","=","$","%"};
+    public static ArrayList<String> fileContent = new ArrayList<>();
 
-        while(scan.hasNextLine()){ //While loop to parse through input file
-            String line = scan.nextLine(); //String variable to gather strings from input file
+    public static void main(String[] args){
+        System.out.println("Please enter a valid filepath : ");
+        Scanner pathScan = new Scanner(System.in);
+        String filePath = pathScan.nextLine();
 
-            for(String val: line.split(" ")){ //For loop that parses through strings and breaks at whitespaces
-                for(int i = 0 ; i < key.length ; i++){ //For loop that parses through keyword array to check for matching tokens
-                    if (val.contains("LET")){
-                        tokens.add("LET");
-                        break;
-                    } else if(val.contains("IF")){
-                        tokens.add("IF_THEN");
-                        break;
-                    } else if(val.contains("FOR")){
-                        tokens.add("FOR_TO");
-                        break;
-                    } else if(val.contains("NEXT")){
-                        tokens.add("NEXT");
-                        break;
-                    } else if(val.contains("GO")){
-                        tokens.add("GOTO");
-                        break;
-                    } else if(val.contains("INPUT")){
-                        tokens.add("INPUT");
-                        break;
-                    } else if(val.contains("PRINT")){
-                        tokens.add("PRINT");
-                        break;
-                    } else if(val.contains("END")){
-                        tokens.add("END");
-                        break;
-                    }
-                }
+        String[] readTokens = readFile(filePath);
+
+        if (readTokens[0] == "INVALID"){
+            System.out.print("Cannot read filepath, terminating program...");
+            System.exit(0);
+        } else if(readTokens[0] == "SYNTAX ERROR") {
+            System.out.print("Cannot define syntax input, terminating program...");
+            System.exit(0);
+        } else {
+            System.out.println("Tokens scanned from input: ");
+            for(int i = 0 ; i < readTokens.length ; i++){
+                System.out.print(readTokens[i] + "|");
             }
+            System.out.print("\nFile Content: " + fileContent);
         }
 
-        System.out.print("Tokens scanned are: " + tokens);
+    }
+
+    public static String[] readFile(String path){
+        try{
+            Scanner fileScan = new Scanner(new File(path));
+            List<String> fContent = new ArrayList<>();
+            String line = "";
+            int counter = 0;
+
+            while(fileScan.hasNextLine()){
+                boolean vLine = true;
+                line = fileScan.nextLine();
+                counter++;
+
+                for(String val : line.split(" ")){
+                    for(int i = 0 ; i < Keywords.length ; i++){
+                        if(val.contains("LET")){
+                            fContent.add("LET");
+                            break;
+                        } else if(val.contains("IF")){
+                            fContent.add("LET");
+                            break;
+                        } else if(val.contains("THEN")){
+                            fContent.add("THEN");
+                            break;
+                        } else if(val.contains("FOR")){
+                            fContent.add("FOR");
+                            break;
+                        } else if(val.contains("TO")){
+                            fContent.add("TO");
+                            break;
+                        } else if(val.contains("NEXT")){
+                            fContent.add("NEXT");
+                            break;
+                        }else if(val.contains("INPUT")){
+                            fContent.add("INPUT");
+                            break;
+                        }else if(val.contains("PRINT")){
+                            fContent.add("PRINT");
+                            break;
+                        }else if(val.contains("\"")){ //This is where I am getting stuck ; I don't need it to print a second STRING token in the final output, just the one
+                            fContent.add("STRING");
+                            break;
+                        }else if(val.contains("END")){
+                            fContent.add("END");
+                            break;
+                        }else if(val.contains("+")){
+                            fContent.add("+");
+                            break;
+                        }else if(val.contains("-")){
+                            fContent.add("-");
+                            break;
+                        }else if(val.contains("*")){
+                            fContent.add("*");
+                            break;
+                        }else if(val.contains("/")){
+                            fContent.add("/");
+                            break;
+                        }else if(val.contains("=")){
+                            fContent.add("=");
+                            break;
+                        }else if(val.contains("%")){
+                            fContent.add("%");
+                            break;
+                        }else if(val.contains("$")){
+                            fContent.add("$");
+                            break;
+                        } else if(val.matches(".*\\d.*")) {
+                            fContent.add("NUM");
+                            break;
+                        } else if(val.matches("\\w+")){
+                            fContent.add("VAR");
+                            break;
+                        }
+                    }
+                }
+                if(vLine != true){
+                    System.out.println("Syntactical Error Detected...");
+                    String[] rArray = {"SYNTAX ERROR"};
+                    return rArray;
+                } else {
+                    fileContent.add(line);
+                }
+            }
+
+            fileScan.close();
+            String[] rArray = fContent.toArray(new String[0]);
+            return rArray;
+        } catch(FileNotFoundException e){
+            System.out.println("Declared filepath is not valid...");
+            String[] rArray = {"INVALID"};
+            return rArray;
+        }
+
     }
 }
 
